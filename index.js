496126eaf161cabc6153c20fd61c201de569461c
gauriwanare <gauriwanare21@gmail.com>
2023-07-11 00:03:39 +0530

    Added pictures

--- index.js
+++
@@ -1,5 +0,0 @@
-const container=document.getElementsByClassName("icon-container");
-container.addEventListner("mousein",()=>{
-    console.log("hovered")
-    document.getElementsByClassName("settings-container").style.display="flex";
-});
